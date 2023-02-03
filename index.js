1647c08010a468cb0e3d2daf6b5d14f01268c195
Shelby Morris <shelbynicole.morris@gmail.com>
2023-02-02 18:33:04 -0700

    completed tests

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // code your solution here
+function saturdayFun(activity = "roller-skate") {
+    return `This Saturday, I want to ${activity}!`;
+}
+ saturdayFun("bathe my dog");
+ function mondayWork(activity = "go to the office") {
+    return `This Monday, I will ${activity}.`
+ }
+function wrapAdjective(flair = "*") {
+    return function(keyWord = "special") {
+        return `You are ${flair}${keyWord}${flair}!`
+    }
+    
+}
